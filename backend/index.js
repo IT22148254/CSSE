88f4e9f4fe28cae4a85fe40921106f6ac146b813
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-10-13 10:33:01 +0530

    cmm

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(express.json()); 
+app.use(cors());        
+
+
+// Database connection
+connectDB();
+
+
+// Import user routes
+// const userRoutes = require('./routes/userRoutes');
+
+
+// Routes
+// app.use('/api/users', userRoutes);
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+// Set up a basic route
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the backend API!');
+// });
+
