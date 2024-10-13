c7401b2914b756e79295f569c49b698fafb5acfa
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-10-13 16:46:41 +0530

    Add controller

--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -1,30 +1,27 @@
 const express = require("express");
+const {
+  createAppointment,
+  getAppointments,
+  getAppointment,
+  deleteAppointment,
+  updateAppointment,
+} = require("../controllers/appointmentController");
 
 const router = express.Router();
 
 // get all appointments
-router.get("/", (req, res) => {
-  res.json({ message: "Get all appoinments" });
-});
+router.get("/", getAppointments);
 
 // get a single appointment
-router.get("/:id", (req, res) => {
-  res.json({ message: "Get a single appointment" });
-});
+router.get("/:id", getAppointment);
 
 // create a new appointment
-router.post("/", (req, res) => {
-  res.json({ message: "Post a new appointment" });
-});
+router.post("/", createAppointment);
 
 // delete a single appointment
-router.delete("/:id", (req, res) => {
-  res.json({ message: "Delete a appointment" });
-});
+router.delete("/:id", deleteAppointment);
 
 // update a single appointment
-router.patch("/:id", (req, res) => {
-  res.json({ message: "Update a appointment" });
-});
+router.patch("/:id", updateAppointment);
 
 module.exports = router;
